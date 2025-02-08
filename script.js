db78ed61cf24ffe050cf416b4ca5fb97355d76e6
AlexHiggManchester <alexander.higginbottom@postgrad.manchester.ac.uk>
2025-02-08 19:46:13 +0000

    updating style a little

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// Update result display when slider is moved
 document.querySelectorAll(".slider").forEach(slider => {
     slider.addEventListener("input", function() {
         this.parentElement.nextElementSibling.querySelector(".result").textContent = this.value;
+    });
+});
+
+// Handle file upload when folder icon is clicked
+document.querySelectorAll(".folder-icon").forEach(icon => {
+    icon.addEventListener("click", function() {
+        this.nextElementSibling.click(); // Opens file dialog
     });
 });
